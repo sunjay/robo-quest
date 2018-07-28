47bea49bc7c9a426d34996f00efc5a49c1627a44
Sunjay Varma <varma.sunjay@gmail.com>
2018-07-27 22:41:33 -0700

    Implementing smoother movement

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ fn main() -> Result<(), String> {
     let canvas_size = canvas.output_size()?;
     let mut robot_center = Point::new(canvas_size.0 as i32 / 2, canvas_size.1 as i32 / 2);
     let mut face_left = false;
+    let mut momentum = 0;
 
     let fps = 60;
     let mut running = true;
@@ -54,13 +55,17 @@ fn main() -> Result<(), String> {
                     running = false;
                 },
                 Event::KeyDown {keycode: Some(Keycode::Right), ..} => {
-                    robot_center = robot_center.offset(3, 0);
+                    momentum = 5;
                     face_left = false;
                 },
                 Event::KeyDown {keycode: Some(Keycode::Left), ..} => {
-                    robot_center = robot_center.offset(-3, 0);
+                    momentum = -5;
                     face_left = true;
                 },
+                Event::KeyUp {keycode: Some(Keycode::Left), ..} |
+                Event::KeyUp {keycode: Some(Keycode::Right), ..} => {
+                    momentum = 0;
+                }
                 _ => {},
             }
         }
@@ -69,6 +74,8 @@ fn main() -> Result<(), String> {
 
         let frames_per_anim_frame = (fps as f64 * (1.0 - robot_animation_speed)) as usize;
         let frames_elapsed = (ticks as f64 / 1000.0 * fps as f64) as usize;
+        robot_center = robot_center.offset(momentum, 0);
+
         // Current frame of the animation
         let robot_animation_frame = (frames_elapsed / frames_per_anim_frame) % robot_animation.len();
 
