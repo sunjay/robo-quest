61d5bc4556cc966f5bd4c2f89d8a441d84610fc3
Sunjay Varma <varma.sunjay@gmail.com>
2018-07-30 16:35:58 -0700

    Implementing keyboard control handling

--- src/main.rs
+++ src/main.rs
@@ -130,8 +130,18 @@ impl<'a> System<'a> for Keyboard {
     type SystemData = (ReadExpect<'a, GameKeys>, WriteStorage<'a, Velocity>, ReadStorage<'a, KeyboardControlled>);
 
     fn run(&mut self, (keys, mut velocities, keyboard_controlled): Self::SystemData) {
-        for (vel, _) in (&mut velocities, &keyboard_controlled).join() {
-            unimplemented!();
+        for (Velocity(ref mut vel), _) in (&mut velocities, &keyboard_controlled).join() {
+            let y = vel.y();
+            // Assuming that only a single arrow key can be held down at a time.
+            if keys.right_arrow {
+                *vel = Point::new(3, y);
+            }
+            else if keys.left_arrow {
+                *vel = Point::new(-3, y);
+            }
+            else {
+                *vel = Point::new(0, y);
+            }
         }
     }
 }
