02544e160dc7c5637786f7982f026be18b9328a1
Sunjay Varma <varma.sunjay@gmail.com>
2018-08-24 16:36:16 -0600

    First failed attempt to try and add movement forces

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -4,9 +4,11 @@ use sdl2::rect::Rect;
 use specs::{Entity, System, Join, ReadExpect, ReadStorage, WriteStorage, Entities};
 use nalgebra::{self as na, Isometry2};
 use nphysics2d::{
-    solver::SignoriniCoulombPyramidModel,
-    object::{BodyHandle, ColliderHandle, Material},
+    solver::{SignoriniCoulombPyramidModel, IntegrationParameters},
+    object::{BodyHandle, ColliderHandle, Material, BodySet},
+    force_generator::ForceGenerator,
     volumetric::Volumetric,
+    math::Force,
     world::World,
 };
 use ncollide2d::shape::{Cuboid, ShapeHandle};
@@ -16,6 +18,29 @@ use resources::FramesElapsed;
 use math::{Vec2D, ToVec2D, ToPoint};
 
 const COLLIDER_MARGIN: f64 = 0.01;
+
+struct ApplyForces {
+    body_forces: Vec<(BodyHandle, Vec2D)>,
+}
+
+impl ForceGenerator<f64> for ApplyForces {
+    fn apply(&mut self, _: &IntegrationParameters<f64>, bodies: &mut BodySet<f64>) -> bool {
+        for &(body_handle, force) in self.body_forces.iter() {
+            if !bodies.contains(body_handle) {
+                // This means that a entity was removed by its AppliedForce component still hung
+                // around somehow.
+                unreachable!("Body was removed from the world before a force could be applied");
+            }
+
+            let mut physics_body = bodies.body_part_mut(body_handle);
+            physics_body.apply_force(&Force::linear(force));
+        }
+
+        // If false, the physics world will remove this force generator after this call.
+        // We don't want this in case world.step() is called more than once.
+        true
+    }
+}
 
 #[derive(Debug)]
 enum Body {
@@ -155,20 +180,35 @@ impl<'a> System<'a> for Physics {
             }
         }
 
-        //TODO: Apply forces: http://nphysics.org/rigid_body_simulations_with_contacts/#gravity-and-external-forces
+        // Apply forces to every rigid body (if any forces have been applied)
+        let body_forces = self.bodies.iter()
+            .filter_map(|(&entity, body)| match (body, applied_forces.get(entity)) {
+                (&Body::RigidBody {body_handle, ..}, Some(&AppliedForce(force))) => {
+                    Some((body_handle, force))
+                },
+                _ => None,
+            })
+            .collect();
+        let force_generator = ApplyForces {body_forces};
+        let force_handle = self.world.add_force_generator(force_generator);
 
         for _ in 0..frames_elapsed {
             self.world.step();
         }
 
+        self.world.remove_force_generator(force_handle);
+
         // Update every tracked entity with the latest values from the physics engine
         // We don't need to update static colliders because they do not move
         for (&entity, body) in self.bodies.iter() {
             if let &Body::RigidBody {body_handle, ..} = body {
-                let position = positions.get_mut(entity).expect("Rigid body should have had a position");
-                let velocity = velocities.get_mut(entity).expect("Rigid body should have had a velocity");
+                let position = positions.get_mut(entity)
+                    .expect("Rigid body should have had a position");
+                let velocity = velocities.get_mut(entity)
+                    .expect("Rigid body should have had a velocity");
 
-                let physics_body = self.world.rigid_body(body_handle).expect("Body handle did not map to a rigid body");
+                let physics_body = self.world.rigid_body(body_handle)
+                    .expect("Body handle did not map to a rigid body");
                 position.0 = physics_body.position().translation.vector.to_point();
                 velocity.0 = physics_body.velocity().linear;
             }
